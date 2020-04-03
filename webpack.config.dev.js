64a9aa692d61092d4ab8e1967b8b053d6ee77e7c
Joseph M <joemark1989@gmail.com>
2020-04-03 18:08:41 -0400

    scafolded the project with new directories, added scss loaders in webpack, updated readme

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -41,7 +41,11 @@ module.exports = {
       {
         test: /(\.css)$/,
         use: ["style-loader", "css-loader"]
-      }
-    ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use:['style-loader','css-loader', 'sass-loader']
+      },
+    ],
   }
 };
