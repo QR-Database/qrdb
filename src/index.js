4c21c76beee955cd4bd669c21865626560635742
Joseph M <joemark1989@gmail.com>
2020-04-03 19:24:04 -0400

    fixed merge conflcts

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import configureStore from "./redux/configureStore";
 import { Provider as ReduxProvider } from "react-redux";
-<<<<<<< HEAD
-import 'materialize-css';
-=======
 import 'materialize-css/dist/css/materialize.min.css'
 
->>>>>>> main-work-branch
 
 const store = configureStore();
 
